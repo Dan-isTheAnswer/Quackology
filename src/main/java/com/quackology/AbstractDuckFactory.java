da0cc06be4667e9ec5e68006815ce40e92295889
Dan <dankim0213@gmail.com>
2020-12-03 14:32:42 +0900

    Abstract Factory Pattern

---
+++ src/main/java/com/quackology/AbstractDuckFactory.java
@@ -0,0 +1,12 @@
+package com.quackology;
+
+public abstract class AbstractDuckFactory {
+
+    public abstract Quackable createMallardDuck();
+    public abstract Quackable createRedheadDuck();
+    public abstract Quackable createDuckCall();
+    public abstract Quackable createRubberDuck();
+}
+
+// Why don't we take the creation of ducks 
+// and localize it in one place?
---
+++ src/main/java/com/quackology/CountingDuckFactory.java
@@ -0,0 +1,27 @@
+package com.quackology;
+
+public class CountingDuckFactory extends AbstractDuckFactory {
+
+    @Override
+    public Quackable createMallardDuck() {
+        return new QuackCounter(new MallardDuck());
+    }
+
+    @Override
+    public Quackable createRedheadDuck() {
+        return new QuackCounter(new RedheadDuck());
+    }
+
+    @Override
+    public Quackable createDuckCall() {
+        return new QuackCounter(new DuckCall());
+    }
+
+    @Override
+    public Quackable createRubberDuck() {
+        return new QuackCounter(new RubberDuck());
+    }
+}
+
+// The simulator will never know the difference; 
+// it just gets back a Quackable.
